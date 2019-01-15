a30db3871371d20c86a0756d056bc9aa1edcc1d7
Gabriel Palmer <gabepalmer@gabriels-macbook-air.local>
2019-01-15 09:28:15 -0700

    Initial Commit

---
+++ SlapJack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlapJack
+//
+//  Created by Gabriel Blaine Palmer on 1/15/19.
+//  Copyright © 2019 Gabriel Blaine Palmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
